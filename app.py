a0864ee772b146d1d12c709304d41503473d07ba
boumster <95254000+boumster@users.noreply.github.com>
2023-02-08 10:54:07 -0700

    Added delete and update

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-from flask import Flask
-from flask import render_template
-from flask import jsonify
-from flask import request
-from flask import redirect
-from flask import url_for
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
@@ -48,6 +43,35 @@ def create_task():
         return jsonify(task)
     
     return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete_task():
+
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+
+    db.session.delete(task)
+    db.session.commit()
+
+
+    return jsonify({'results': 'Ok'}), 200
+
+
+@app.route('/complete', methods=['POST'])
+def complete_task():
+
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+
+    task.completed = True
+
+    db.session.add(task)
+    db.session.commit()
+
+    return jsonify({'results': 'Ok'}), 200
+
+
 
 if __name__ == '__main__':
     app.run()
--- static/main.js
+++ static/main.js
@@ -33,18 +33,13 @@ const TaskApp = {
           },
 
         async getTasks(){
-            const response = await fetch(window.location, {
-                method: 'get',
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            })
-
+            const response = await this.sendRequest(window.location, 'get')
             this.tasks = await response.json()
 
         },
         
         async createTask(){
+            
             await this.getTasks()
       
             await this.sendRequest(window.location + 'create', 'post', JSON.stringify(this.task))
@@ -52,7 +47,23 @@ const TaskApp = {
             await this.getTasks()
       
             this.task.title = ''
+          },
+
+          async deleteTask(task){
+            await this.sendRequest(window.location + 'delete', 'post', JSON.stringify(task))
+
+            await this.getTasks()
+
+          },
+
+          async completeTask(task){
+
+            await this.sendRequest(window.location + 'complete', 'post', JSON.stringify(task))
+
+            await this.getTasks()
           }
+
+
     },
 
     delimiters: ['{', '}'],
